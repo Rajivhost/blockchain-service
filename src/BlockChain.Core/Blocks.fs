aa0002b73c4e7c2d0a846c2f467ced2fd1ecc72b
Rajiv Mounguengue <rajiv.mounguengue@functionalstack.io>
2019-04-07 16:47:39 +0100

    Added BlockChain objects.

--- src/BlockChain.Core/Blocks.fs
+++ src/BlockChain.Core/Blocks.fs
@@ -10,6 +10,15 @@ type Transaction =
       RecipientAddress : string
       Amount : decimal }
 
+module Transaction =
+    let empty =
+        { SenderAddress = String.Empty
+          RecipientAddress = String.Empty
+          Amount = 0m }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Assembling the block
+////////////////////////////////////////////////////////////////////////////////////////////////////
 type Block =
     { Index : int
       TimeStamp : DateTime
@@ -40,9 +49,111 @@ module Block =
         let leadingZeros = new string('0', difficulty)
         let mutable nonce = block.Nonce
         let mutable hash = block.Hash
-
-        while Object.ReferenceEquals(hash, null) || hash.Substring(0, difficulty) <> leadingZeros do
+        while Object.ReferenceEquals(hash, null)
+              || hash.Substring(0, difficulty) <> leadingZeros do
             nonce <- nonce + 1
-            hash <- { block with Nonce = nonce; Hash = hash } |> calculateHash
+            hash <- { block with Nonce = nonce
+                                 Hash = hash }
+                    |> calculateHash
+        { block with Nonce = nonce
+                     Hash = hash }
 
-        { block with Nonce = nonce; Hash = hash }
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Assembling the block chain
+////////////////////////////////////////////////////////////////////////////////////////////////////
+type Blockchain =
+    { PendingTransactions : Transaction list
+      Chain : Block list
+      Difficulty : int
+      Reward : decimal }
+
+module Blockchain =
+    let empty =
+        { PendingTransactions = List.Empty
+          Chain = List.Empty
+          Difficulty = 2
+          Reward = 1m }
+
+    let createGenesisBlock blockChain =
+        let genesisBlock = (Block.empty, blockChain.Difficulty) |> Block.mine
+        genesisBlock
+
+    let addGenesisBlock blockChain =
+        let genesisBlock = blockChain |> createGenesisBlock
+        let chain = blockChain.Chain @ [ genesisBlock ]
+        let blockChain = { blockChain with Chain = chain }
+        blockChain
+
+    let getLatestBlock blockChain =
+        let latestBlock = blockChain.Chain |> List.last
+        latestBlock
+
+    let addTransaction (blockChain, transaction) =
+        let pendingTransactions =
+            blockChain.PendingTransactions @ [ transaction ]
+        let blockChain =
+            { blockChain with PendingTransactions = pendingTransactions }
+        blockChain
+
+    let addBlock (blockChain, block) =
+        let latestBlock = blockChain |> getLatestBlock
+
+        let block =
+            { block with Index = latestBlock.Index + 1
+                         PreviousHash = latestBlock.Hash }
+
+        let block = (block, blockChain.Difficulty) |> Block.mine
+        let chain = blockChain.Chain @ [ block ]
+        let blockChain = { blockChain with Chain = chain }
+        blockChain
+
+    let processPendingTransactions (blockChain, minerAddress) =
+        let latestBlock = blockChain |> getLatestBlock
+
+        let block =
+            { Block.empty with PreviousHash = latestBlock.Hash
+                               Transactions = blockChain.PendingTransactions }
+
+        let blockChain = (blockChain, block) |> addBlock
+        let blockChain = { blockChain with PendingTransactions = List.empty }
+
+        // COINBASE
+        let transaction =
+            { Transaction.empty with RecipientAddress = minerAddress
+                                     Amount = blockChain.Reward }
+
+        let blockChain = (blockChain, transaction) |> addTransaction
+        blockChain
+
+    let isValid blockChain =
+        let mutable result = false
+        for index in 1..blockChain.Chain |> List.length do
+            let currentBlock = blockChain.Chain.[index]
+            let previousBlock = blockChain.Chain.[index - 1]
+            match (currentBlock.Hash <> (currentBlock |> Block.calculateHash),
+                   currentBlock.PreviousHash <> previousBlock.Hash) with
+            | false, false -> result <- false
+            | false, _ -> result <- false
+            | _, false -> result <- false
+            | _ -> result <- true
+        result
+
+    let getBalance (blockChain, address) =
+        let transactions =
+            blockChain.Chain
+            |> List.map (fun block -> block.Transactions)
+            |> List.fold (fun acc value -> acc @ value) []
+
+        let debitTransactionsAmount =
+            transactions
+            |> List.where
+                   (fun transaction -> transaction.SenderAddress = address)
+            |> List.sumBy (fun transaction -> transaction.Amount)
+
+        let creditTransactionsAmount =
+            transactions
+            |> List.where
+                   (fun transaction -> transaction.RecipientAddress = address)
+            |> List.sumBy (fun transaction -> transaction.Amount)
+
+        creditTransactionsAmount - debitTransactionsAmount
